5a3b026c4e5002b3929c502cdd0c6fc5fa4fd7c4
Aashrit Luthra <aashrit@bolt.com>
2021-08-10 11:08:26 -0400

    solved two array problems

---
+++ Questions/Arrays/twoSum.py
@@ -0,0 +1,7 @@
+def twoSum(nums: List[int], target: int) -> List[int]:
+    seen = {}
+    for i in range(len(nums)):
+        if target - nums[i] in seen:
+            return [seen[target-nums[i]],i]
+        seen[nums[i]] = i
+    return [-1,-1]
